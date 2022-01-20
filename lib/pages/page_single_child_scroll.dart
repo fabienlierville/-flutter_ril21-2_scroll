ee4aececb915f24d35f0a13367618c958d9856b7
Fabien LIERVILLE <flierville@cesi.fr>
2022-01-20 11:37:34 +0100

    Single Child

--- lib/pages/page_single_child_scroll.dart
+++ lib/pages/page_single_child_scroll.dart
@@ -5,6 +5,47 @@ class PageSingleChildScroll extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Single Child Scroll"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(height: 100,color: Colors.red,),
+            Container(height: 100,color: Colors.blue,),
+            Container(height: 100,color: Colors.green,),
+            Container(height: 100,color: Colors.yellow,),
+            Container(height: 100,color: Colors.white,),
+            Container(height: 100,color: Colors.black38,),
+            Container(height: 100,color: Colors.red,),
+            Container(height: 100,color: Colors.blue,),
+            Container(height: 100,color: Colors.green,),
+            Container(height: 100,color: Colors.yellow,),
+            Container(height: 100,color: Colors.white,),
+            Container(height: 100,color: Colors.black38,),
+            Container(height: 100,color: Colors.red,),
+            Container(height: 100,color: Colors.blue,),
+            Container(height: 100,color: Colors.green,),
+            Container(height: 100,color: Colors.yellow,),
+            Container(height: 100,color: Colors.white,),
+            Container(height: 100,color: Colors.black38,),
+            Container(height: 100,color: Colors.red,),
+            Container(height: 100,color: Colors.blue,),
+            Container(height: 100,color: Colors.green,),
+            Container(height: 100,color: Colors.yellow,),
+            Container(height: 100,color: Colors.white,),
+            Container(height: 100,color: Colors.black38,),
+            Container(height: 100,color: Colors.red,),
+            Container(height: 100,color: Colors.blue,),
+            Container(height: 100,color: Colors.green,),
+            Container(height: 100,color: Colors.yellow,),
+            Container(height: 100,color: Colors.white,),
+            Container(height: 100,color: Colors.black38,),
+
+          ],
+        ),
+      ),
+    );
   }
 }
