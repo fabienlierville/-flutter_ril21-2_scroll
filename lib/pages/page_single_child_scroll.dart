89cf718ef8ddac0eb377d6870a422f80a641153c
Fabien LIERVILLE <flierville@cesi.fr>
2022-01-20 11:32:27 +0100

    first

---
+++ lib/pages/page_single_child_scroll.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PageSingleChildScroll extends StatelessWidget {
+  const PageSingleChildScroll({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
